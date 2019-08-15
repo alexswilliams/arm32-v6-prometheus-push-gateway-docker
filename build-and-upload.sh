982e89b387904546661bd00b2642d1482c98e7f5
Alex Williams <alex.williams@skybettingandgaming.com>
2019-08-16 00:04:00 +0100

    Updated versions

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -5,19 +5,27 @@ set -ex
 function buildAndPush {
     local version=$1
     local imagename="alexswilliams/arm32v6-prometheus-push-gateway"
-    local fromline=$(grep -e '^FROM ' Dockerfile.arm32v6 | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/[ \t]+.*//')
+    local fromline=$(grep -e '^FROM ' Dockerfile | tail -n -1 | sed 's/^FROM[ \t]*//' | sed 's#.*/##' | sed 's/:/-/' | sed 's/#.*//' | sed -E 's/ +.*//')
 
     docker build -t ${imagename}:${version} \
         --build-arg VERSION=${version} \
         --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
         --build-arg VCS_REF=$(git rev-parse --short HEAD) \
-        --file Dockerfile.arm32v6 . \
+        --file Dockerfile . \
     && docker tag ${imagename}:${version} ${imagename}:${version}-${fromline} \
     && docker push ${imagename}:${version} \
-    && docker push ${imagename}:${version}-${fromline}
+    && docker push ${imagename}:${version}-${fromline} \
+    && (
+        if [ "$2" == "latest" ]; then
+            docker tag ${imagename}:${version} ${imagename}:latest \
+            && docker push ${imagename}:latest
+        fi
+    )
 }
 
-buildAndPush "0.7.0"
-buildAndPush "0.8.0"
+# buildAndPush "0.7.0"
+# buildAndPush "0.8.0"
+buildAndPush "0.9.0"
+buildAndPush "0.9.1" latest
 
 curl -X POST "https://hooks.microbadger.com/images/alexswilliams/arm32v6-prometheus-push-gateway/Xrunc81LwVPUfwFs_wAMFS9kffQ="
